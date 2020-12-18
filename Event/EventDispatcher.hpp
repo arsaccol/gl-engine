aa90a75f8b7407a8450f5a3dcf5071376c536652
Alexandre R. Saccol <arsaccol@gmail.com>
2020-12-17 21:04:29 -0300

    Move EventDispatcher method definitions outside class declaration

--- Event/EventDispatcher.hpp
+++ Event/EventDispatcher.hpp
@@ -25,28 +25,10 @@ public:
 public:
 
 	template <typename EventSubType>
-	void RegisterHandler(const EventHandler& handler)
-	{
-		eventHandlers.insert(std::pair<std::type_index, EventHandler>(typeid(EventSubType), handler));
-	}
-
+	void RegisterHandler(const EventHandler& handler);
 
 	template <typename T>
-	void DispatchEvent(const Event& event)
-	{
-		// I'm not quite sure this cast will work as intended...
-		const T& derivedEvent = static_cast<const T&>(event);
-
-		// equal_range gives us begin and end iterators for the range of values under the given key (i.e. event type)
-		std::pair<HandlerCollection::iterator, HandlerCollection::iterator> pairs =
-			eventHandlers.equal_range(typeid(derivedEvent));
-
-
-		for (auto iter = pairs.first; iter != pairs.second; ++iter)
-		{
-			iter->second(derivedEvent);
-		}
-	}
+	void DispatchEvent(const Event& event);
 
 	static EventDispatcher& Instance()
 	{
@@ -56,5 +38,30 @@ public:
 private:
 	HandlerCollection eventHandlers;
 	static EventDispatcher instance;
-
 };
+
+
+
+// ============= Template method implementations =============
+template <typename EventSubType>
+void EventDispatcher::RegisterHandler(const EventHandler& handler)
+{
+	eventHandlers.insert(std::pair<std::type_index, EventHandler>(typeid(EventSubType), handler));
+}
+
+template <typename T>
+void EventDispatcher::DispatchEvent(const Event& event)
+{
+	// I'm not quite sure this cast will work as intended...
+	const T& derivedEvent = static_cast<const T&>(event);
+
+	// equal_range gives us begin and end iterators for the range of values under the given key (i.e. event type)
+	std::pair<HandlerCollection::iterator, HandlerCollection::iterator> pairs =
+		eventHandlers.equal_range(typeid(derivedEvent));
+
+
+	for (auto iter = pairs.first; iter != pairs.second; ++iter)
+	{
+		iter->second(derivedEvent);
+	}
+}
