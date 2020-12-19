5e7007396715a3db3ef4cfb8d5cc00d0190241e9
Alexandre R. Saccol <arsaccol@gmail.com>
2020-12-18 23:37:59 -0300

    Add comments on top of Observer class

--- Event/Observer.hpp
+++ Event/Observer.hpp
@@ -3,6 +3,12 @@
 #include "EventDispatcher.hpp"
 #include <functional>
 
+// Inherit from this and implement void EventHandler(const Event&)
+// to have an observer that will respond to EventType events.
+// ============================================================
+// I'm not sure this is the right way to do it. Maybe we should
+// just give observers an std::function and register it with the
+// EventDispatcher in the observer's constructor?
 template <typename EventType>
 class Observer
 {
