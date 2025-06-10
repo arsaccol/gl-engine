0070342b74529fd8f0e27505ad9d28ef67a06ca1
Alexandre R. Saccol <arsaccol@gmail.com>
2025-06-10 08:54:45 -0300

    remove comments

--- src/Prefabs/Factory.hpp
+++ src/Prefabs/Factory.hpp
@@ -59,30 +59,18 @@ struct Factory
 		entt::entity parent,
 		const Light& lightProperties,
 		const glm::vec3& position = glm::vec3(0.0f)
-		// Scale and orientation are not directly settable via current Transform constructor
-		// const glm::vec3& scale = glm::vec3(1.0f),
-		// const glm::quat& orientation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f)
 	)
 	{
 		entt::entity lightEntity = registry.create();
 
-		// Add Transform component (using constructor that takes position)
 		registry.emplace<Transform>(lightEntity, position);
-		// If scale/orientation need to be set, it would be done here:
-		// auto& transform = registry.get<Transform>(lightEntity);
-		// transform.scale = scale; // Assuming Transform has a scale member
-		// transform.orientation = orientation;
-
-		// Add Light component
 		registry.emplace<Light>(lightEntity, lightProperties);
 
-		// Add SceneNode component and attach to parent
 		registry.emplace<SceneNode>(lightEntity);
 		if (registry.valid(parent)) {
 			SceneNode::addChild(registry, parent, lightEntity);
 		}
 		// Else: it becomes a root object or handle error/warning if parent was expected
-
 		return lightEntity;
 	}
 
